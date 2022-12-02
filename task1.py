d6d6ad7fe1c9cb28cb1065499ceb85c9f0f8a18e
unknown <Dmitiy_yar@mail.ru>
2022-12-02 09:55:56 +0300

    add task1

---
+++ task1.py
@@ -0,0 +1,22 @@
+#  Вычислить число Пи c заданной точностью d
+
+
+from decimal import Decimal, getcontext
+
+number = int(input('Введите количество знаков  '))
+
+def numberPi(n):
+    getcontext().prec = n
+    res = Decimal(0)
+    for i in range(n):
+        a = Decimal(1/(16**i))
+        b = Decimal(4/(8*i+1))
+        c = Decimal(2/(8*i+4))
+        d = Decimal(1/(8*i+5))
+        e = Decimal(1/(8*i+6))
+        r = a*(b-c-d-e)
+        res += r
+    return res
+
+print(numberPi(number))
+    
